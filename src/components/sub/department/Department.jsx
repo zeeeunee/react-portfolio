94bb72352b03daa0785bfab5b71a00d1a8bfe0de
zeeeunee <wldmstv@gmail.com>
2023-12-04 16:13:30 +0900

    history추가

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -7,6 +7,8 @@ export default function Department() {
 	const [HistoryData, setHistoryData] = useState([]);
 	const [MemberTit, setMemberTit] = useState('');
 	const [MemberData, setMemberData] = useState([]);
+	const [YellowImage, setYellowImage] = useState([]);
+
 	const path = process.env.PUBLIC_URL;
 
 	const fetchDepartment = () => {
@@ -27,14 +29,43 @@ export default function Department() {
 			});
 	};
 
+	const fetchYellow = () => {
+		fetch(`${path}/DB/yellow.json`)
+			.then((data) => data.json())
+			.then((json) => {
+				setYellowImage(Object.values(json)[0]);
+			});
+	};
+
 	useEffect(() => {
 		fetchDepartment();
 		fetchHistory();
+		fetchYellow();
 	}, []);
 
 	return (
 		<Layout title={'Department'}>
-			<h2>{HistoryTit}</h2>
+			{/* <h2>{HistoryTit}</h2> */}
+
+			<div className='historyYellow'>
+				<section className='yellow'>
+					{YellowImage.map((yellow, idx) => {
+						return (
+							<article key={yellow + idx}>
+								<img src={`${path}/img/${yellow.pic}`} alt={yellow.pic} />
+							</article>
+						);
+					})}
+				</section>
+				<section className='yellowRightBox'>
+					<h2>History Our Creation</h2>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente fuga molestias, suscipit omnis quis vel accusantium rerum debitis
+						ullam! Voluptatem consequatur quidem libero maiores, expedita beatae unde autem corrupti cum doloribus velit deserunt! Perspiciatis,
+						reprehenderit dignissimos delectus maxime vitae illo!
+					</p>
+				</section>
+			</div>
 			<section className='historyBox'>
 				<div className='con'>
 					{HistoryData.map((history, idx) => {
@@ -52,9 +83,10 @@ export default function Department() {
 					})}
 				</div>
 			</section>
-			<h2>{MemberTit}</h2>
-			<h3>A team United by shared values</h3>
+			{/* <h2>{MemberTit}</h2> */}
+
 			<section className='memberBox'>
+				<h3>A team United by shared values</h3>
 				<div className='leftBox'>
 					<h3>6</h3>
 					<p>Professional members in our team</p>
