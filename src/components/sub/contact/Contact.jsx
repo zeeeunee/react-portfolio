cfa347ab557f2f9f7aff6c52d94685bc3e9162f7
zeeeunee <wldmstv@gmail.com>
2023-12-14 22:49:16 +0900

    마커 생성

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -1,17 +1,22 @@
 import { useEffect, useRef } from 'react';
 import Layout from '../../common/layout/Layout';
 import './Contact.scss';
-
 export default function Contact() {
 	const mapFrame = useRef(null);
 	const { kakao } = window;
-	const mapOption = useRef({ center: new kakao.maps.LatLng(33.450701, 126.570667), level: 3 });
+	const mapOption = useRef({ center: new kakao.maps.LatLng(37.511553, 127.022009), level: 3 });
+
+	const imageSrc = process.env.PUBLIC_URL + `/img/marker1.png`;
+	const imageSize = new kakao.maps.Size(242, 99);
+	const imageOption = { offset: new kakao.maps.Point(116, 99) };
 
 	useEffect(() => {
 		const mapInstance = new kakao.maps.Map(mapFrame.current, mapOption.current);
-		const posInstance = new kakao.maps.LatLng(33.450701, 126.570667);
+		const markerImageInstance = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
+		const posInstance = mapOption.current.center;
 		const markerInstance = new kakao.maps.Marker({
-			position: posInstance
+			position: posInstance,
+			image: markerImageInstance
 		});
 		markerInstance.setMap(mapInstance);
 	}, []);
