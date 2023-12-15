a14f7c383d7f7576819c5cc063e530ea2a35417e
zeeeunee <wldmstv@gmail.com>
2023-12-15 09:25:51 +0900

    지도 위치 가운데 갱신

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -3,45 +3,51 @@ import Layout from '../../common/layout/Layout';
 import './Contact.scss';
 
 export default function Contact() {
-	const { kakao } = window;
+	const kakao = useRef(window.kakao);
 	const [Index, setIndex] = useState(0);
 	const mapFrame = useRef(null);
 	const marker = useRef(null);
+	const mapInstance = useRef(null);
+
 	const mapInfo = useRef([
 		{
 			title: '이케아 고양점',
-			latlng: new kakao.maps.LatLng(37.629957, 126.862974),
+			latlng: new kakao.current.maps.LatLng(37.629957, 126.862974),
 			imgSrc: `${process.env.PUBLIC_URL}/img/marker1.png`,
-			imgSize: new kakao.maps.Size(232, 99),
-			imgPos: { offset: new kakao.maps.Point(116, 99) }
+			imgSize: new kakao.current.maps.Size(232, 99),
+			imgPos: { offset: new kakao.current.maps.Point(116, 99) }
 		},
 		{
 			title: '이케아 광명점',
-			latlng: new kakao.maps.LatLng(37.42452, 126.881864),
+			latlng: new kakao.current.maps.LatLng(37.42452, 126.881864),
 			imgSrc: `${process.env.PUBLIC_URL}/img/marker2.png`,
-			imgSize: new kakao.maps.Size(232, 99),
-			imgPos: { offset: new kakao.maps.Point(116, 99) }
+			imgSize: new kakao.current.maps.Size(232, 99),
+			imgPos: { offset: new kakao.current.maps.Point(116, 99) }
 		},
 		{
 			title: '이케아 기흥점',
-			latlng: new kakao.maps.LatLng(37.222253, 127.116268),
+			latlng: new kakao.current.maps.LatLng(37.222253, 127.116268),
 			imgSrc: `${process.env.PUBLIC_URL}/img/marker3.png`,
-			imgSize: new kakao.maps.Size(232, 99),
-			imgPos: { offset: new kakao.maps.Point(116, 99) }
+			imgSize: new kakao.current.maps.Size(232, 99),
+			imgPos: { offset: new kakao.current.maps.Point(116, 99) }
 		}
 	]);
 
-	marker.current = new kakao.maps.Marker({
+	marker.current = new kakao.current.maps.Marker({
 		position: mapInfo.current[Index].latlng,
-		image: new kakao.maps.MarkerImage(mapInfo.current[Index].imgSrc, mapInfo.current[Index].imgSize, mapInfo.current[Index].imgOpt)
+		image: new kakao.current.maps.MarkerImage(mapInfo.current[Index].imgSrc, mapInfo.current[Index].imgSize, mapInfo.current[Index].imgOpt)
 	});
 
+	const setCenter = () => mapInstance.current.setCenter(mapInfo.current[Index].latlng);
+
 	useEffect(() => {
-		const mapInstance = new kakao.maps.Map(mapFrame.current, {
+		mapInstance.current = new kakao.current.maps.Map(mapFrame.current, {
 			center: mapInfo.current[Index].latlng,
 			level: 3
 		});
-		marker.current.setMap(mapInstance);
+		marker.current.setMap(mapInstance.current);
+		window.addEventListener('resize', setCenter);
+		return () => window.removeEventListener('resize', setCenter);
 	}, [Index, kakao]);
 
 	return (
