6c6d972bee35b717cb6c9cf2b1be45ac5161f43b
zeeeunee <wldmstv@gmail.com>
2023-12-25 12:54:12 +0900

    header스타일링 완료

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,50 +1,61 @@
 import './Header.scss';
 import { NavLink, Link } from 'react-router-dom';
 import { TiThMenu } from 'react-icons/ti';
-
-export default function Header({ Toggle, setToggle, Dark, setDark }) {
+import { useDispatch, useSelector } from 'react-redux';
+import * as types from '../../../redux/action';
+export default function Header({ Dark, setDark }) {
+	const dispatch = useDispatch();
+	const Toggle = useSelector(store => store.menuReducer.menu);
 	return (
 		<header className='Header'>
 			<h1>
 				<Link to='/'>ZEEEUNEE</Link>
 			</h1>
-			<ul>
-				<li>
-					<NavLink to='/department' activeClassName={'on'}>
-						Department
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to='/youtube' activeClassName={'on'}>
-						Youtube
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to='/gallery' activeClassName={'on'}>
-						Gallery
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to='/community' activeClassName={'on'}>
-						Community
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to='/members' activeClassName={'on'}>
-						Members
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to='/contact' activeClassName={'on'}>
-						Contact
-					</NavLink>
-				</li>
-			</ul>
-			<button className='menuToggle' onClick={() => setToggle(!Toggle)}>
-				<TiThMenu className='menuIcon' />
-			</button>
-			<div className={`themeBox ${Dark && 'dark'}`} onClick={() => setDark(!Dark)}>
-				<div className='ball'></div>
+
+			<div className='menu'>
+				<div className='darkToggle'>
+					<div className={`themeBox ${Dark && 'dark'}`} onClick={() => setDark(!Dark)}>
+						<div className='ball'></div>
+					</div>
+				</div>
+				<ul>
+					<li>
+						<NavLink to='/department' activeClassName={'on'}>
+							Department
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/youtube' activeClassName={'on'}>
+							Youtube
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/gallery' activeClassName={'on'}>
+							Gallery
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/community' activeClassName={'on'}>
+							Community
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/members' activeClassName={'on'}>
+							Members
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/contact' activeClassName={'on'}>
+							Contact
+						</NavLink>
+					</li>
+				</ul>
+				<button className='menuToggle' onClick={() => dispatch({ type: types.MENU.start, payload: !Toggle })}>
+					<TiThMenu className='menuIcon' />
+				</button>
+				{/* <div className={`themeBox ${Dark && 'dark'}`} onClick={() => setDark(!Dark)}>
+					<div className='ball'></div>
+				</div> */}
 			</div>
 		</header>
 	);
