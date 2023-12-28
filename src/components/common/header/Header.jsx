16726204a464baf342ba117403f2cc97171b45a3
zeeeunee <wldmstv@gmail.com>
2023-12-28 16:22:02 +0900

    header스타일링수정

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -11,44 +11,48 @@ export default function Header() {
 			<h1>
 				<Link to='/'>ZEEEUNEE</Link>
 			</h1>
-			<ul>
-				<li>
-					<NavLink to='/department' activeClassName={'on'}>
-						Department
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to='/youtube' activeClassName={'on'}>
-						Youtube
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to='/gallery' activeClassName={'on'}>
-						Gallery
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to='/community' activeClassName={'on'}>
-						Community
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to='/members' activeClassName={'on'}>
-						Members
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to='/contact' activeClassName={'on'}>
-						Contact
-					</NavLink>
-				</li>
-			</ul>
-			<button className='menuToggle' onClick={() => setMenuOpen(!MenuOpen)}>
-				<TiThMenu className='menuIcon' />
-			</button>
-			<div className={`themeBox ${Dark && 'dark'}`} onClick={() => setDark(!Dark)}>
-				<div className='ball'></div>
-			</div>
+			<section className='menu'>
+				<div className='darkToggle'>
+					<div className={`themeBox ${Dark && 'dark'}`} onClick={() => setDark(!Dark)}>
+						<div className='ball'></div>
+					</div>
+				</div>
+				<ul>
+					<li>
+						<NavLink to='/department' activeClassName={'on'}>
+							Department
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/youtube' activeClassName={'on'}>
+							Youtube
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/gallery' activeClassName={'on'}>
+							Gallery
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/community' activeClassName={'on'}>
+							Community
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/members' activeClassName={'on'}>
+							Members
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/contact' activeClassName={'on'}>
+							Contact
+						</NavLink>
+					</li>
+				</ul>
+				<button className='menuToggle' onClick={() => setMenuOpen(!MenuOpen)}>
+					<TiThMenu className='menuIcon' />
+				</button>
+			</section>
 		</header>
 	);
 }
