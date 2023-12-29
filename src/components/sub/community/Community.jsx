c5cc9545a473b48f871384a5b0733a1bbfdaa5a5
zeeeunee <wldmstv@gmail.com>
2023-12-29 09:27:54 +0900

    더미데이터 보이게 하기

--- src/components/sub/community/Community.jsx
+++ src/components/sub/community/Community.jsx
@@ -2,7 +2,7 @@ import Layout from '../../common/layout/Layout';
 import './Community.scss';
 import { useEffect, useRef, useState } from 'react';
 import { useCustomText } from '../../../hooks/useText';
-import './dummyPosts.json';
+import postData from './dummyPosts.json';
 
 export default function Community() {
 	const changeText = useCustomText('combined');
