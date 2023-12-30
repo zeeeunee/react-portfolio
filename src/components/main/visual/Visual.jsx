2d3f9b623abadb8c22a9168b84d83934b910f7b9
zeeeunee <wldmstv@gmail.com>
2023-12-30 18:22:16 +0900

    swiper기능, 스타일 추가

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -1,5 +1,76 @@
+import { useYoutubeQuery } from '../../../hooks/useYoutubeQuery';
 import './Visual.scss';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
+import { Pagination, Autoplay } from 'swiper';
+import { useEffect } from 'react';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import { useCustomText } from '../../../hooks/useText';
+
+function Btns() {
+	const swiper = useSwiper();
+
+	useEffect(() => {
+		swiper.slideNext(300);
+	}, [swiper]);
+
+	return (
+		<nav className='swiperController'>
+			<button
+				onClick={() => {
+					swiper.slideNext(300);
+					swiper.autoplay.start();
+				}}>
+				start
+			</button>
+			<button onClick={() => swiper.autoplay.stop()}>stop</button>
+		</nav>
+	);
+}
 
 export default function Visual() {
-	return <figure className='Visual'></figure>;
+	const { data: youtube, isSuccess } = useYoutubeQuery();
+	const shortenText = useCustomText('shorten');
+	return (
+		<figure className='Visual'>
+			<Swiper
+				modules={[Pagination, Autoplay]}
+				pagination={{
+					clickable: true,
+					renderBullet: (index, className) => {
+						return `<span class=${className}>${index + 1}</span>`;
+					}
+				}}
+				autoplay={{
+					delay: 2000,
+					disableOnInteraction: true
+				}}
+				loop={true}>
+				{isSuccess &&
+					youtube.map((vid, idx) => {
+						if (idx >= 5) return null;
+						return (
+							<SwiperSlide key={vid.id}>
+								<div className='inner'>
+									<div className='picBox'>
+										<p>
+											<img src={vid.snippet.thumbnails.standard.url} alt={vid.snippet.title} />
+										</p>
+										<p>
+											<img src={vid.snippet.thumbnails.standard.url} alt={vid.snippet.title} />
+										</p>
+									</div>
+									<div className='txtBox'>
+										<h2>{shortenText(vid.snippet.title, 40)}</h2>
+										<Link to={`/detail/${vid.id}`}>View Detail</Link>
+									</div>
+								</div>
+							</SwiperSlide>
+						);
+					})}
+				<Btns />
+			</Swiper>
+		</figure>
+	);
 }
