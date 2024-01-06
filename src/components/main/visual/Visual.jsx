daddaba8ee4dd25994bc41a77fe1fb706ed6d33c
zeeeunee <wldmstv@gmail.com>
2024-01-06 15:57:06 +0900

    유튜브 미리보기 화면 출력, 다음이미지버튼 클릭 이벤트추가

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -1,33 +1,64 @@
 import { useYoutubeQuery } from '../../../hooks/useYoutubeQuery';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper';
 import './Visual.scss';
 import 'swiper/css';
 import { useRef, useState } from 'react';
 
 export default function Visual() {
+	const num = useRef(5);
+	const swipeRef = useRef(null);
 	const { isSuccess, data } = useYoutubeQuery();
-	const [Index, setIndex] = useState(0);
-	console.log(Index);
+	const [PrevIndex, setPrevIndex] = useState(1);
+	const [Index, setIndex] = useState(2);
+	const [NextIndex, setNextIndex] = useState(3);
 
 	const swiperOpt = useRef({
+		modules: [Autoplay],
 		loop: true,
 		slidesPerView: 1,
 		spaceBetween: 50,
 		centeredSlides: true,
-		onSwiper: swiper => swiper.slideNext(300),
-		onSlideChange: swiper => setIndex(swiper.realIndex),
+		onSwiper: swiper => (swipeRef.current = swiper),
+		onSlideChange: swiper => {
+			setIndex(swiper.realIndex);
+			swiper.realIndex === 0 ? setPrevIndex(num.current - 1) : setPrevIndex(swiper.realIndex - 1);
+			swiper.realIndex === num.current - 1 ? setNextIndex(0) : setNextIndex(swiper.realIndex + 1);
+		},
+		autoplay: { delay: 2000, disableOnInteraction: true },
 		breakpoints: {
 			1000: { slidesPerView: 2 },
 			1400: { slidesPerView: 3 }
 		}
 	});
+
+	const trimTitle = title => {
+		let resultTit = '';
+		if (title.includes('(')) resultTit = title.split('(')[0];
+		else if (title.includes('[')) resultTit = title.split('[')[0];
+		else resultTit = title;
+		return resultTit;
+	};
+
 	return (
 		<figure className='Visual'>
-			<div className='txtBox'></div>
+			<div className='txtBox'>
+				<ul>
+					{isSuccess &&
+						data.map((el, idx) => {
+							if (idx >= 5) return null;
+							return (
+								<li key={el.id} className={idx === Index ? 'on' : ''}>
+									<h3>{trimTitle(el.snippet.title)}</h3>
+								</li>
+							);
+						})}
+				</ul>
+			</div>
 			<Swiper {...swiperOpt.current}>
 				{isSuccess &&
 					data.map((el, idx) => {
-						if (idx >= 5) return null;
+						if (idx >= num.current) return null;
 						return (
 							<SwiperSlide key={el.id}>
 								<div className='pic'>
@@ -42,6 +73,18 @@ export default function Visual() {
 						);
 					})}
 			</Swiper>
+			<nav className='preview'>
+				{isSuccess && (
+					<>
+						<p className='prevBox' onClick={() => swipeRef.current.slidePrev(400)}>
+							<img src={data[PrevIndex].snippet.thumbnails.default.url} alt={data[PrevIndex].snippet.title} />
+						</p>
+						<p className='nextBox' onClick={() => swipeRef.current.slideNext(400)}>
+							<img src={data[NextIndex].snippet.thumbnails.default.url} alt={data[NextIndex].snippet.title} />
+						</p>
+					</>
+				)}
+			</nav>
 		</figure>
 	);
 }
