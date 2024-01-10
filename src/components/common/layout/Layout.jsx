e93bc44dbb73ed657a6f0217b528951bac571f58
zeeeunee <wldmstv@gmail.com>
2024-01-10 17:35:57 +0900

    btnTop버튼 만들고 useThrottle에 담기

--- src/components/common/layout/Layout.jsx
+++ src/components/common/layout/Layout.jsx
@@ -2,6 +2,7 @@ import { useEffect, useRef } from 'react';
 import './Layout.scss';
 import { useSplitText } from '../../../hooks/useSplitText';
 import Anime from '../../../asset/anime';
+import { useThrottle } from '../../../hooks/useThrottle';
 
 export default function Layout({ children, title }) {
 	const refFrame = useRef(null);
@@ -13,15 +14,20 @@ export default function Layout({ children, title }) {
 		const scroll = window.scrollY;
 		scroll >= 200 ? btnTop.current.classList.add('on') : btnTop.current.classList.remove('on');
 	};
+
+	const throttledhandleScroll = useThrottle(handleScroll);
+
 	useEffect(() => {
-		window.addEventListener('scroll', handleScroll);
-	}, []);
+		window.addEventListener('scroll', throttledhandleScroll);
+	}, [throttledhandleScroll]);
+
 	useEffect(() => {
 		splitText(refTitle.current, title, 0.7, 0.1);
 		setTimeout(() => {
 			refFrame.current?.classList.add('on');
 		}, 300);
 	}, [splitText, title]);
+
 	return (
 		<main ref={refFrame} className={`Layout ${title}`}>
 			<h1 ref={refTitle}>{title}</h1>
