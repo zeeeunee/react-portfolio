827e826236ae29cdc54f75a2c9d0043ad92772e5
zeeeunee <wldmstv@gmail.com>
2024-02-03 20:30:21 +0900

    메인에 스크롤버튼 추가

--- src/components/main/mainWrap/MainWrap.jsx
+++ src/components/main/mainWrap/MainWrap.jsx
@@ -1,10 +1,12 @@
 import Pics from '../pics/Pics';
 import Visual from '../visual/Visual';
+import Btns from '../btns/Btns';
 import './MainWrap.scss';
 
 export default function MainWrap() {
 	return (
 		<div className='MainWrap'>
+			<Btns />
 			<Visual />
 			<Pics />
 		</div>
