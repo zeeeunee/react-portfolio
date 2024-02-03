1e5ed0e661cb86481b392293395a5978464bad0f
zeeeunee <wldmstv@gmail.com>
2024-02-03 19:54:09 +0900

    메인에 info추가

--- src/components/main/mainWrap/MainWrap.jsx
+++ src/components/main/mainWrap/MainWrap.jsx
@@ -1,5 +1,6 @@
 import Banner from '../banner/Banner';
 import Btns from '../btns/Btns';
+import Info from '../info/Info';
 import Pics from '../pics/Pics';
 import Visual from '../visual/Visual';
 import './MainWrap.scss';
@@ -11,6 +12,7 @@ export default function MainWrap() {
 			<Visual />
 			<Pics />
 			<Banner />
+			<Info />
 		</div>
 	);
 }
