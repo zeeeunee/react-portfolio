6f0b2773f563f93342b8a1fb015f66dc481668bd
zeeeunee <wldmstv@gmail.com>
2024-02-03 18:51:28 +0900

    layout 컴포넌트에 top버튼 기능추가, 스타일추가

--- src/components/common/layout/Layout.jsx
+++ src/components/common/layout/Layout.jsx
@@ -1,11 +1,26 @@
 import { useEffect, useRef } from 'react';
 import './Layout.scss';
 import { useSplitText } from '../../../hooks/useText';
+import Anime from '../../../asset/anime';
+import { useThrottle } from '../../../hooks/useThrottle';
 
 export default function Layout({ children, title }) {
 	const refFrame = useRef(null);
 	const refTitle = useRef(null);
 	const splitText = useSplitText();
+	const btnTop = useRef(null);
+
+	const handleScroll = () => {
+		const scroll = window.scrollY;
+		scroll >= 200 ? btnTop.current?.classList.add('on') : btnTop.current?.classList.remove('on');
+	};
+
+	const throttledhandleScroll = useThrottle(handleScroll);
+
+	useEffect(() => {
+		window.addEventListener('scroll', throttledhandleScroll);
+	}, [throttledhandleScroll]);
+
 	useEffect(() => {
 		splitText(refTitle.current, title, 0.7, 0.1);
 		setTimeout(() => {
@@ -16,6 +31,14 @@ export default function Layout({ children, title }) {
 		<main ref={refFrame} className={`Layout ${title}`}>
 			<h1 ref={refTitle}>{title}</h1>
 			{children}
+			<button
+				ref={btnTop}
+				className='btnTop'
+				onClick={() => {
+					new Anime(window, { scroll: 0 });
+				}}>
+				top
+			</button>
 		</main>
 	);
 }
