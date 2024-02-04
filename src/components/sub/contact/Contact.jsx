68ac0c721bb7d96dd4397b479198e80e6ea00f77
zeeeunee <wldmstv@gmail.com>
2024-02-04 09:48:07 +0900

    contact 컴포넌트 throttling 처리

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -5,6 +5,7 @@ import emailjs from '@emailjs/browser';
 import { FaMapMarkerAlt } from 'react-icons/fa';
 import { BiSolidPhone } from 'react-icons/bi';
 import { MdEmail } from 'react-icons/md';
+import { useThrottle } from '../../../hooks/useThrottle';
 
 export default function Contact() {
 	const form = useRef();
@@ -13,7 +14,6 @@ export default function Contact() {
 		const elArr = form.current.children;
 
 		Array.from(elArr).forEach(el => {
-			console.log(el);
 			if (el.name === 'user_name' || el.name === 'user_email' || el.name === 'message') el.value = '';
 		});
 	};
@@ -23,15 +23,15 @@ export default function Contact() {
 		const [user, email] = form.current.querySelectorAll('input');
 		const txtArea = form.current.querySelector('textarea');
 
-		if (!user.value || !email.value || !txtArea.value) return alert('이름, 이메일주소 문의내용을 모두 입력하세요.');
+		if (!user.value || !email.value || !txtArea.value) return alert('Please fill in all the contents.');
 
 		emailjs.sendForm('service_1gzi4dl', 'template_8l9j4yu', form.current, 'SbGYfi4rN5zFsWjvZ').then(
 			result => {
-				alert('문의 내용이 성공적으로 전송되었습니다.');
+				alert('Successfully transmitted.');
 				resetForm();
 			},
 			error => {
-				alert('일시적인 장애로 문의 전송에 실패했습니다. 다음의 메일주소로 보내주세요.');
+				alert('Transmission failed. Please send it to the following e-mail address.');
 				resetForm();
 			}
 		);
@@ -62,7 +62,7 @@ export default function Contact() {
 			imgPos: { offset: new kakao.current.maps.Point(116, 99) }
 		},
 		{
-			title: 'IKEA Gi-heumg',
+			title: 'IKEA Gi-heung',
 			latlng: new kakao.current.maps.LatLng(37.222474, 127.114621),
 			imgSrc: `${process.env.PUBLIC_URL}/img/marker3.png`,
 			imgSize: new kakao.current.maps.Size(232, 99),
@@ -74,18 +74,20 @@ export default function Contact() {
 		position: mapInfo.current[Index].latlng,
 		image: new kakao.current.maps.MarkerImage(mapInfo.current[Index].imgSrc, mapInfo.current[Index].imgSize, mapInfo.current[Index].imgOpt)
 	});
-	const roadview = useRef(() => {
+	const roadview = useCallback(() => {
 		new kakao.current.maps.RoadviewClient().getNearestPanoId(mapInfo.current[Index].latlng, 50, panoId => {
 			new kakao.current.maps.Roadview(viewFrame.current).setPanoId(panoId, mapInfo.current[Index].latlng);
 		});
-	});
+	}, [Index]);
 	const setCenter = useCallback(() => {
 		mapInstance.current.setCenter(mapInfo.current[Index].latlng);
-		roadview.current();
 	}, [Index]);
+
+	const throttledSetCenter = useThrottle(setCenter);
 
 	useEffect(() => {
 		mapFrame.current.innerHTML = '';
+		viewFrame.current.innerHTML = '';
 		mapInstance.current = new kakao.current.maps.Map(mapFrame.current, {
 			center: mapInfo.current[Index].latlng,
 			level: 3
@@ -94,16 +96,21 @@ export default function Contact() {
 		setTraffic(false);
 		setView(false);
 
-		roadview.current();
-
 		mapInstance.current.addControl(new kakao.current.maps.MapTypeControl(), kakao.current.maps.ControlPosition.TOPRIGHT);
 
 		mapInstance.current.addControl(new kakao.current.maps.ZoomControl(), kakao.current.maps.ControlPosition.RIGHT);
 
 		mapInstance.current.setZoomable(false);
-		window.addEventListener('resize', setCenter);
-		return () => window.removeEventListener('resize', setCenter);
-	}, [Index, setCenter]);
+	}, [Index, roadview]);
+
+	useEffect(() => {
+		window.addEventListener('resize', throttledSetCenter);
+		return () => window.removeEventListener('resize', throttledSetCenter);
+	}, [throttledSetCenter]);
+
+	useEffect(() => {
+		View && viewFrame.current.children.length === 0 && roadview();
+	}, [View, roadview]);
 
 	useEffect(() => {
 		Traffic
@@ -147,7 +154,7 @@ export default function Contact() {
 			</div>
 			<div id='mailSection'>
 				<div className='mailLeft'>
-					<h3>Message Us</h3>
+					<h3>MESSAGE US</h3>
 					<p className='p1'>
 						Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae non voluptatum deleniti amet id? Qui quo voluptates quis tempore
 						impedit?
@@ -158,9 +165,9 @@ export default function Contact() {
 					</p>
 				</div>
 				<form ref={form} onSubmit={sendEmail}>
-					<input type='text' name='user_name' placeholder='Name' />
-					<input type='email' name='user_email' placeholder='Email' />
-					<textarea name='message' placeholder='Message' />
+					<input type='text' name='user_name' placeholder='NAME' />
+					<input type='email' name='user_email' placeholder='EMAIL' />
+					<textarea name='message' placeholder='MESSAGE' />
 					<input className='emailButton' type='submit' value='Send' />
 				</form>
 			</div>
